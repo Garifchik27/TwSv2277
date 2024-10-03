73bc53ef2b65bc32b3daa85b0252b4b5e12d5de4
Garifchik27 <165847456+Garifchik27@users.noreply.github.com>
2024-10-03 14:00:29 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TwSv2277
+# 27 is a club, whoever needs to, they will enter "You don't know who to pretend to be, I'm the "King and Jester"
---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+// alert("Hello World");
+
+// let one = prompt("Введите первое слагаемое");
+// let two = prompt("Введите второе слагаемое");
+// one = Number(one);
+// two = Number(two);
+// alert(one + "+" + two + "=" + (one + two));
+
+// console.log(userName, age);
+
+// console.log(10+3);
+// console.log(10-3);
+// console.log(10*3);
+// console.log(10/3);
+// console.log(10**3);
+// console.log(10%3);
+
+let age = +prompt("Сколько вам лет?");
+alert(`${2024 - age} вы года рождения`);
